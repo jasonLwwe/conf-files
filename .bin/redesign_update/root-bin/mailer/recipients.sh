ad106a5856fca7ca2157fb74e97eb8de706044c6
jasonLwwe <jason.lyerly@wwecorp.com>
2016-01-07 11:38:14 -0500

    Tweaked crontab file and removed root-bin.

--- .bin/redesign_update/root-bin/mailer/recipients.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash 
-
-echo Jason.Lyerly@wwecorp.com, \
-Joe.Janos@wwecorp.com, \
-dpugliese@devspark.com, \
-Tom.Fabbricante@wwecorp.com, \
-Suguna.Sanakkayala@wwecorp.com, \
-Sandeep.Mahadik@wwecorp.com
-
