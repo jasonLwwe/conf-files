a79038eaad75c9d8b9be7d6eb5be8d0d35247336
jasonLwwe <jason.lyerly@wwecorp.com>
2017-02-12 15:24:45 -0500

    Added db refresh script

---
+++ .bin/refreshdb.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+oldpwd=$(pwd)
+
+cd ~
+
+if [ -f wwe.sql.bz2 ]; then
+  echo A db file already exists at $(pwd)/wwe.sql.bz2 ;
+  echo Please delete it, or rename it if you wish to keep it, and try again. ;
+  exit;
+fi
+
+echo -n "Downloading new stage file backup... "
+wget http://stage-dbback.cloud.wwe.com/wwe.sql.bz2 &> /dev/null
+echo done
+
+echo -n "Dropping database wwe... "
+mysql -e 'drop database wwe;'
+echo done
+
+echo -n "Creating new database wwe... "
+mysql -e 'create database wwe;'
+echo done
+
+echo -n "Importing new database... "
+lbzcat wwe.sql.bz2 | mysql -D wwe
+echo done
+
+if [ -d /u01/www/wwe/html ]
+then
+  echo "Moving to /u01/www/wwe/html";
+  cd /u01/www/wwe/html ;
+else
+  echo "Moing to /u01/www/wwe3redesign";
+  cd /u01/www/wwe3redesign/html ;
+fi
+
+echo "Enabling stage file proxy... "
+drush en -y stage_file_proxy
+echo ...done
+
+echo  "Setting stage_file_proxy_origin"
+drush vset stage_file_proxy_origin http://www.wwwe.com
+echo ...done
+
+echo  "Doing drush updatedb... "
+drush updatedb -y
+echo ...done
+
+echo "Clearing drupal cache... "
+drush cc all 
+echo ...done
+
+cd $oldpwd
+
